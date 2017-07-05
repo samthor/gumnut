459084e47f9d44d579af15b2d8377bc0c2150c31
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-05 18:40:19 +1000

    split out replaced stdlib

--- wasm/wasm.sh
+++ wasm/wasm.sh
@@ -7,5 +7,5 @@ emcc -O0 -s WASM=1 -s ONLY_MY_CODE=1 \
   -s LIBRARY_DEPS_TO_AUTOEXPORT="[]" \
   -s EXPORTED_FUNCTIONS="['_prsr_setup', '_prsr_run', '_prsr_get_at', '_prsr_get_line_no', '_prsr_get_len', '_prsr_get_type']" \
   -o runner.js \
-  runner.c ../*.c
+  *.c ../*.c
 
