fba7e2d4d0f3b97de69d5e4f0e8b9e18efdb5e2d
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-05 18:40:26 +1000

    more features in display

---
+++ wasm/utils.js
@@ -0,0 +1,27 @@
+
+async function createWebAssembly(path, importObject) {
+  const bytes = await window.fetch(path).then(x => x.arrayBuffer());
+  const object = await WebAssembly.instantiate(bytes, importObject);
+  object.importObject = importObject;
+  return object;
+}
+
+function encodeToArray(s, array) {
+  let i = 0;
+  for (let si = 0; si < s.length; ++si) {
+    const c = s.charCodeAt(si);
+    if (c >= 128) {
+      throw new Error('non-ascii not yet supported: ' + c);
+    }
+    array[i++] = c;
+  }
+  array[i++] = 0;
+}
+
+function copyToMemory(memory, s) {
+  const array = new Uint8Array(memory.buffer);
+  const startAt = array.length - ((s.length + 1) * 4);
+  const writeTo = new Uint8Array(memory.buffer, startAt);
+  encodeToArray(s, writeTo);
+  return startAt;
+}
