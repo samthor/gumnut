f61a5ae9ba9a7328ff7385cb4e357a6c609f4eda
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-06 15:01:46 +1000

    wasm release script

---
+++ wasm/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+./wasm.sh
+
+mkdir -p ../dist
+cp runner.wasm index.html utils.js ../dist
+
+git checkout gh-pages
+cp ../dist/* ..
+
+git add ../
+git commit -m "release on $(date)"
+
