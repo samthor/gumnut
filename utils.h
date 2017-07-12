3b66f5039ea83b73477a65814eea193d41a2c588
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-12 18:56:50 +1000

    labellike

--- utils.h
+++ utils.h
@@ -21,6 +21,7 @@ int is_control_keyword(char *s, int len);
 int is_asi_keyword(char *s, int len);
 int is_hoist_keyword(char *s, int len);
 int is_expr_keyword(char *s, int len);
+int is_labellike_keyword(char *s, int len);
 int is_begin_expr_keyword(char *s, int len);
 int is_op_keyword(char *s, int len);
 int is_decl_keyword(char *s, int len);
