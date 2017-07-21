d3d992370e62db1b79d7a01f9ee5455097009c3a
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-21 17:03:48 +1000

    define exported functions in code

--- wasm/runner.c
+++ wasm/runner.c
@@ -2,34 +2,44 @@
 #include "../token.h"
 #include "../parser.h"
 
+// this is just EMSCRIPTEN_KEEPALIVE from the actual source
+#define EXPORT __attribute__((used))
+
 static parserdef shared_parser;
 static token out;
 
+EXPORT
 int prsr_setup(char *buf) {
   prsr_parser_init(&shared_parser, buf);
   return 0;
 }
 
+EXPORT
 int prsr_run() {
   return prsr_next(&shared_parser, &out);
 }
 
+EXPORT
 int prsr_get_at() {
   return out.p - shared_parser.td.buf;
 }
 
+EXPORT
 int prsr_get_len() {
   return out.len;
 }
 
+EXPORT
 int prsr_get_invalid() {
   return out.invalid;
 }
 
+EXPORT
 int prsr_get_line_no() {
   return out.line_no;
 }
 
+EXPORT
 int prsr_get_type() {
   return out.type;
 }
