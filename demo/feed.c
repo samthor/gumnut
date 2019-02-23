6e2742c1ce2b906d5f5310cd2d054ab3bd5ce8ad
Sam Thorogood <sam.thorogood@gmail.com>
2019-02-23 16:46:06 +1100

    moar

--- demo/feed.c
+++ demo/feed.c
@@ -5,6 +5,7 @@
 
 #define _PAREN_FOR   1
 #define _PAREN_CATCH 2
+#define _PAREN_DEF   3
 
 #define _ret(x) {int _c = (x); if (_c) { return _c; }}
 
@@ -20,6 +21,19 @@ typedef struct {
 static int state__slist(feeddef *fd);
 static int state__s(feeddef *fd);
 
+static int is_decl_start(token *curr, token *next) {
+  if (curr->type != TOKEN_LIT || !is_decl_keyword(curr->p, curr->len)) {
+    return 0;
+  }
+
+  if (curr->p[0] != 'l' || next->type == TOKEN_BRACE || next->type == TOKEN_ARRAY) {
+    // const, var or e.g. "let[..]" or "let{..}", destructuring
+  } else if (next->type != TOKEN_LIT || is_op_keyword(next->p, next->len)) {
+    return 0;  // no following literal (e.g. "let = 1", "instanceof" counts as op)
+  }
+
+  return 1;
+}
 
 static int token_string(token *t, char *s, int len) {
   return t->len == len && !memcmp(t->p, s, len);
@@ -64,9 +78,16 @@ static int state__value(feeddef *fd, int is_next) {
     _ret(_read(fd, 0));
   }
 
-  // FIXME: values are just numbers, for now
-  if (fd->curr.type == TOKEN_NUMBER) {
-    return 0;
+  for (;;) {
+    switch (fd->curr.type) {
+      case TOKEN_STRING:
+      case TOKEN_REGEXP:
+      case TOKEN_NUMBER:
+        // FIXME: values are just numbers, for now
+        return 0;
+    }
+
+    break;
   }
 
   printf("got unhandled value: %d\n", fd->curr.type);
@@ -138,6 +159,23 @@ static int _read_ensure_semicolon(feeddef *fd) {
   return ERROR__SYNTAX;
 }
 
+static int state__block(feeddef *fd, int is_next) {
+  if (is_next) {
+    _ret(_read(fd, 0));
+  }
+  if (fd->curr.type != TOKEN_BRACE) {
+    return ERROR__SYNTAX;
+  }
+
+  _ret(state__slist(fd));
+  _ret(_read(fd, 0));
+
+  if (fd->curr.type != TOKEN_CLOSE || fd->curr.p[0] != '}') {
+    return ERROR__SYNTAX;
+  }
+  return 0;
+}
+
 static int state__paren(feeddef *fd, int flag) {
   if (fd->next->type != TOKEN_PAREN) {
     if (flag == _PAREN_CATCH) {
@@ -152,8 +190,16 @@ static int state__paren(feeddef *fd, int flag) {
   if (flag == _PAREN_FOR) {
     // 1st part of for loop
     if (fd->next->type != TOKEN_SEMICOLON) {
-      // TODO: also match "var ..."
-      _ret(state__value(fd, 1));
+      _read(fd, 0);
+
+      // match declaration start "for (var x = ..."
+      if (is_decl_start(&(fd->curr), fd->next)) {
+        fd->curr.type = TOKEN_KEYWORD;
+        _ret(state__decl(fd));
+      } else {
+        // otherwise it's just a value
+        _ret(state__value(fd, 0));
+      }
     }
     _read(fd, 0);
 
@@ -166,6 +212,12 @@ static int state__paren(feeddef *fd, int flag) {
     // 3rd part of for loop
     if (fd->next->type != TOKEN_CLOSE) {
       _ret(state__value(fd, 1));
+    }
+  } else if (flag == _PAREN_DEF) {
+    // "function foo (x=123, y=456)"
+    if (fd->next->type != TOKEN_CLOSE) {
+      // can close immediately here
+      _ret(state__decl(fd));
     }
   } else if (flag == _PAREN_CATCH) {
     // read decl-like thing "catch ([x,y]) {}"
@@ -183,21 +235,55 @@ static int state__paren(feeddef *fd, int flag) {
   return 0;
 }
 
-static int state__block(feeddef *fd, int is_next) {
-  if (is_next) {
-    _ret(_read(fd, 0));
-  }
-  if (fd->curr.type != TOKEN_BRACE) {
-    return ERROR__SYNTAX;
+static int state__value_function(feeddef *fd) {
+  // always starts "on" function
+  // FIXME: basic functions only, no async/*
+
+  if (fd->next->type == TOKEN_LIT) {
+    _read(fd, 0);
+
+    if (is_always_keyword(fd->curr.p, fd->curr.len)) {
+      return ERROR__SYNTAX;
+    }
   }
 
-  _ret(state__slist(fd));
-  _ret(_read(fd, 0));
+  _ret(state__paren(fd, _PAREN_DEF));
+  return state__block(fd, 1);
+}
 
-  if (fd->curr.type != TOKEN_CLOSE || fd->curr.p[0] != '}') {
-    return ERROR__SYNTAX;
+static int state__value_class(feeddef *fd) {
+  // always starts "on" class
+
+  if (fd->next->type == TOKEN_LIT) {
+    _read(fd, 0);
+    int extends = 0;
+
+    if (token_string(&(fd->curr), "extends", 7)) {
+      fd->curr.type = TOKEN_KEYWORD;
+      extends = 1;
+    } else if (is_always_keyword(fd->curr.p, fd->curr.len)) {
+      return ERROR__SYNTAX;
+    } else {
+      fd->curr.type = TOKEN_SYMBOL;
+
+      if (fd->next->type == TOKEN_LIT && token_string(fd->next, "extends", 7)) {
+        _read(fd, 0);
+        fd->curr.type = TOKEN_KEYWORD;
+        extends = 1;
+      }
+    }
+
+    // optionally consume what we're extending
+    if (extends) {
+      // FIXME: this should only be a single value, doesn't allow e.g. 1+1
+      // FIXME: but allows "new foo" (seemingly nothing else)
+      _ret(state__value(fd, 1));
+    }
   }
-  return 0;
+
+  // TODO: consume class body
+  _read(fd, 0);
+  return ERROR__TODO;
 }
 
 static int state__s(feeddef *fd) {
@@ -220,10 +306,9 @@ restart_s:
       break;
     }
 
-    // FIXME: `let` is not always keyword, fix later
-    if (is_decl_keyword(fd->curr.p, fd->curr.len)) {
+    // match var/const/let (sometimes)
+    if (is_decl_start(&(fd->curr), fd->next)) {
       fd->curr.type = TOKEN_KEYWORD;
-
       _ret(state__decl(fd));
       return _read_ensure_semicolon(fd);
     }
@@ -358,8 +443,25 @@ restart_s:
       // nb. "do ; while(0)" does not require a trailing semicolon or ASI
     }
 
-    if (token_string(&(fd->curr), "function", 8)) {
+    // hoisted class
+    if (token_string(&(fd->curr), "class", 5)) {
+      fd->curr.type = TOKEN_KEYWORD;
 
+      if (fd->next->type != TOKEN_LIT || token_string(fd->next, "extends", 7)) {
+        _read(fd, 0);
+        return ERROR__SYNTAX;  // hoisted class must have name
+      }
+      return state__value_class(fd);
+    }
+
+    // "async" (as optional precursor to function)
+    int has_async = 0;
+    if (token_string(&(fd->curr), "async", 5)) {
+      has_async = 1;
+    }
+
+    if (token_string(&(fd->curr), "function", 8)) {
+      return state__value_function(fd);
     }
 
   } while (0);
