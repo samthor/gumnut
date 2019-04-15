c7abe111ef85f389c5ad4a43c341bc9e0541b256
Sam Thorogood <sam.thorogood@gmail.com>
2019-04-15 16:25:23 +1000

    better release

--- wasm/release.sh
+++ wasm/release.sh
@@ -10,6 +10,6 @@ cp runner.wasm index.html utils.js ../dist
 git checkout gh-pages
 cp ../dist/* ..
 
-git add ../
+git add $(find .. -type f -maxdepth 1)  # only files in top-level dir
 git commit -m "release on $(date)"
 
