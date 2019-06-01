64978339ee15e75d67f790b88587a249f29673d7
Sam Thorogood <sam.thorogood@gmail.com>
2019-06-01 23:05:30 +1000

    optimize

--- token.c
+++ token.c
@@ -499,7 +499,7 @@ static void eat_next(tokendef *d) {
   d->next.len = eat.len;
 
   // special-case token
-  switch (d->next.type) {
+  switch (eat.type) {
     case TOKEN_EOF:
       d->next.line_no = 0;  // always change line_no for EOF
       break;
