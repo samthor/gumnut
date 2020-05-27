f1b524ebde3e7f526f88161dd24c726baea72284
Sam Thorogood <sam.thorogood@gmail.com>
2020-05-27 16:37:22 +1000

    neaten up resolver

--- wasm/resolver.js
+++ wasm/resolver.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
+/*
+ * Copyright 2020 Sam Thorogood.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 import fs from 'fs';
 import path from 'path';
 import build from './wrap.js';
 
-function resolve(s) {
-  return ':foo:' + s;
-}
-
 const PENDING_BUFFER_MAX = 1024 * 16;
 
-export default async function resolver() {
+export default async function resolver(resolve) {
   const source = path.join(path.dirname(import.meta.url.split(':')[1]), 'runner.wasm');
   const wasm = fs.readFileSync(source);
 
@@ -18,15 +29,16 @@ export default async function resolver() {
   const run = await build(wasm);
 
   return (f, stream) => {
-    // TODO: use readSync to read directly into target buffer
-    const buffer = fs.readFileSync(f);
-
     const fd = fs.openSync(f);
     const stat = fs.fstatSync(fd);
 
-    const prepare = (buffer) => {
+    let buffer = null;
+    const prepare = (b) => {
+      buffer = b;  // store for later
       const read = fs.readSync(fd, buffer, 0, stat.size, 0);
-      console.info('read', read, 'of', stat.size);
+      if (read !== stat.size) {
+        throw new Error(`did not read all bytes at once: ${read}/${stat.size}`);
+      }
     };
 
     let sent = 0;
@@ -47,11 +59,23 @@ export default async function resolver() {
       // bump to high water mark
       stream.write(buffer.subarray(sent, p));
 
-      const view = buffer.subarray(p + 1, p + len - 1);
-      const s = decoder.decode(view);  // TODO: could have " or etc within
+      // const c = String.fromCharCode(buffer[p]);
+      // process.stderr.write('got: ' + c + '\n');
 
-      const out = resolve(s);
-//      process.stderr.write(`???found import/export: ${s}, ${out}\n`);
+      const view = buffer.subarray(p + 1, p + len - 1);
+      let s;
+
+      if (view.some((c) => c === 92)) {
+        // take the slow train, choo choo
+        console.warn('got slash', s);
+
+        
+
+      } else {
+        s = decoder.decode(view);
+      }
+
+      const out = resolve(s, f);
 
       stream.write(JSON.stringify(out), 'utf-8');
       sent = p + len;  // we've moved past the "original" string here
@@ -64,7 +88,9 @@ export default async function resolver() {
 
 
 (async function () {
-  const r = await resolver();
+  const r = await resolver((importee, importer) => {
+    return `:blah:${importee}`;
+  });
   r(process.argv[2], process.stdout);
 }());
 
