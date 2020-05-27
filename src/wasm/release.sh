50cc1f49c71ca7e521a6ad9b56a087de480920d0
Sam Thorogood <sam.thorogood@gmail.com>
2020-05-27 23:16:39 +1000

    release for subdir

--- src/wasm/release.sh
+++ src/wasm/release.sh
@@ -10,15 +10,15 @@ git checkout master
 ./wasm.sh release
 
 # copy to temporary location
-mkdir -p ../dist
-cp runner.wasm index.html utils.js ../dist
+mkdir -p ../../dist
+cp runner.wasm index.html utils.js ../../dist
 
 # move into place
 git checkout gh-pages
-cp ../dist/* ..
+cp ../../dist/* ../..
 
 # add files
-git add $(find .. -type f -maxdepth 1)  # only files in top-level dir
+git add $(find ../.. -type f -maxdepth 1)  # only files in top-level dir
 git commit -m "release on $(date)"
 git push
 
